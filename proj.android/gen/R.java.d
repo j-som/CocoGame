c0f5b477174e2ffc8632f8d44860ead12c7e7706
j-som <308401057@qq.com>
2015-12-12 13:47:30 +0800

    reorgnize

---
+++ proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+E:\jsom\CocoGame\proj.android\gen\com\jsom\game\R.java \
+ : E:\jsom\CocoGame\proj.android\res\drawable-hdpi\icon.png \
+E:\jsom\CocoGame\proj.android\res\drawable-ldpi\icon.png \
+E:\jsom\CocoGame\proj.android\res\drawable-mdpi\icon.png \
+E:\jsom\CocoGame\proj.android\res\values\strings.xml \
+E:\jsom\CocoGame\proj.android\bin\AndroidManifest.xml \
